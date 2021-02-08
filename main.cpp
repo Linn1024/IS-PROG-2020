359a6aeca92dff5af7f73878d8ec394013520252
Linn1024 <linn1024@gmail.com>
2021-02-08 23:30:38 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 int main() {
-    Point p1(0, 0);
-    Point p2(1, 1);
-    Point p3(0 , 2);
-    Point* a = new Point[3] {p1, p2, p3};
-    Triangle t(3, a);
+//    Point p1(0, 0);
+//    Point p2(1, 1);
+//    Point p3(0 , 2);
+//    Point* a = new Point[3] {p1, p2, p3};
+//    Triangle t(3, a);
+    cout << "Hello world";
 //    cout << t.hasRightAngle();
 }
