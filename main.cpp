cae590a0c49ebaa2209fee2ee81e02133265c200
Linn1024 <linn1024@gmail.com>
2021-02-08 23:26:33 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "geometry.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Point p1(0, 0);
+    Point p2(1, 1);
+    Point p3(0 , 2);
+    Point* a = new Point[3] {p1, p2, p3};
+    Triangle t(3, a);
+//    cout << t.hasRightAngle();
+}
