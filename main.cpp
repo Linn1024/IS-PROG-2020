538f0043773156029609b2a150e2d73439900781
Linn1024 <linn1024@gmail.com>
2021-02-08 23:39:08 +0300

    fake tests

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-//#include "geometry.h"
+#include "geometry.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-//    Point p1(0, 0);
-//    Point p2(1, 1);
-//    Point p3(0 , 2);
-//    Point* a = new Point[3] {p1, p2, p3};
-//    Triangle t(3, a);
-    cout << "Hello world";
-//    cout << t.hasRightAngle();
+    Point p1(0, 0);
+    Point p2(1, 1);
+    Point p3(0 , 2);
+    Point* a = new Point[3] {p1, p2, p3};
+    Triangle t(3, a);
+//    cout << "Hello world";
+    assert(t.hasRightAngle());
 }
