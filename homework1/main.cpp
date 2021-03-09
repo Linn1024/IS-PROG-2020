4026b31f03ea1f4a9b597ae64d60d42c52af434c
Linn1024 <linn1024@gmail.com>
2021-03-09 13:16:44 +0300

    fixed test

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -46,6 +46,8 @@ void PolygonsCheck() {
     Trapezoid trCopy(*trap);
     delete trap;
     assert(abs(trCopy.height() - 4) < EPS);
+    Triangle floatArea(3, new Point[3]{Point(0, 0), Point(1, 1), Point(0, 1)});
+    assert(floatArea.area() == 0.5);
 }
 
 int main() {
